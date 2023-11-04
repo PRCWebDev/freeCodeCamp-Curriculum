45fa5c7bc0bdfefc61bcdff435164ca42c45fc90
PRCWebDev <webdevprc@gmail.com>
2023-11-04 13:40:51 +0200

    completed more challenges from the Basic JavaScript Course

--- JavaScript Algorithms and Data Structures Certification/Basic JavaScript/script.js
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/script.js
@@ -229,38 +229,74 @@ const remainder = 11 % 3;
 
 ////////////////////////////////////////
 //  ? Concatenating Strings with the Plus Equals Operator
+// let myStr = 'This is the first sentence. ';
+// myStr += 'This is the second sentence.';
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Constructing Strings with Variables
+// Only change code below this line
+// const myName = 'Radu';
+// const myStr = 'My name is ' + myName + ' and I am well!';
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Appending Variables to Strings
+// Change code below this line
+// const someAdjective = 'lorem ipsum';
+// let myStr = 'Learning to code is ';
+// myStr += someAdjective;
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Find the Length of a String
+// Setup
+// let lastNameLength = 0;
+// const lastName = 'Lovelace';
+// // Only change code below this line
+// lastNameLength = lastName.length;
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Use Bracket Notation to Find the First Character in a String
+// // Setup
+// let firstLetterOfLastName = "";
+// const lastName = "Lovelace";
+// // Only change code below this line
+// firstLetterOfLastName = lastName[0]; // Change this line
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Understand String Immutability
+// // Setup
+// let myStr = "Jello World";
+// // Only change code below this line
+// myStr = "Hello World"; // Change this line
+// // Only change code above this line
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Use Bracket Notation to Find the Nth Character in a String
+// // Setup
+// const lastName = "Lovelace";
+// // Only change code below this line
+// const thirdLetterOfLastName = lastName[2]; // Change this line
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Use Bracket Notation to Find the Last Character in a String
+// // Setup
+// const lastName = 'Lovelace';
+// // Only change code below this line
+// const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Use Bracket Notation to Find the Nth-to-Last Character in a String
+// // Setup
+// const lastName = "Lovelace";
+// // Only change code below this line
+// const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
 ////////////////////////////////////////
 
 ////////////////////////////////////////
