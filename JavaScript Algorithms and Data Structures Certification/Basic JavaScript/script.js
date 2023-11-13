150010cacf89c0f504508952b7fb8bacb7d86e9f
PRCWebDev <webdevprc@gmail.com>
2023-11-13 10:42:19 +0200

    completed more challenges

--- JavaScript Algorithms and Data Structures Certification/Basic JavaScript/script.js
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/script.js
@@ -943,38 +943,164 @@ function trueOrFalse(wasThatTrue) {
 
 ////////////////////////////////////////
 //  ? Return Early Pattern for Functions
+// // Setup
+// function abTest(a, b) {
+//   // Only change code below this line
+//   if (a * b < 0) return undefined;
+//   // Only change code above this line
+
+//   return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+// }
+
+// abTest(2, 2);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
-//  ? Counting Cards
+// //  ? Counting Cards
+// let count = 0;
+
+// function cc(card) {
+//   // Only change code below this line
+//   switch (card) {
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//     case 6:
+//       count++;
+//       break;
+//     case 10:
+//     case 'J':
+//     case 'Q':
+//     case 'K':
+//     case 'A':
+//       count--;
+//       break;
+//   }
+
+//   if (count > 0) {
+//     return count + ' Bet';
+//   } else {
+//     return count + ' Hold';
+//   }
+//   // Only change code above this line
+// }
+
+// cc(2);
+// cc(3);
+// cc(7);
+// cc('K');
+// cc('A');
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Build JavaScript Objects
+// const myDog = {
+//   // Only change code below this line
+//   name: 'Lorem',
+//   legs: 3,
+//   tails: 1,
+//   friends: ['Ipsum', 'Dolor', 'Sit'],
+//   // Only change code above this line
+// };
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Accessing Object Properties with Dot Notation
+// // Setup
+// const testObj = {
+//   hat: 'ballcap',
+//   shirt: 'jersey',
+//   shoes: 'cleats',
+// };
+
+// // Only change code below this line
+// const hatValue = testObj.hat; // Change this line
+// const shirtValue = testObj.shirt; // Change this line
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Accessing Object Properties with Bracket Notation
+// // Setup
+// const testObj = {
+//   'an entree': 'hamburger',
+//   'my side': 'veggies',
+//   'the drink': 'water',
+// };
+
+// // Only change code below this line
+// const entreeValue = testObj['an entree']; // Change this line
+// const drinkValue = testObj['the drink']; // Change this line
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Accessing Object Properties with Variables
+// const dogs = {
+//   Fido: 'Mutt',
+//   Hunter: 'Doberman',
+//   Snoopie: 'Beagle',
+// };
+
+// const myDog = 'Hunter';
+// const myBreed = dogs[myDog];
+// console.log(myBreed);
+
+// // Setup
+// const testObj = {
+//   12: 'Namath',
+//   16: 'Montana',
+//   19: 'Unitas',
+// };
+
+// // Only change code below this line
+// const playerNumber = 16; // Change this line
+// const player = testObj[playerNumber]; // Change this line
+// console.log(player);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Updating Object Properties
+// // Setup
+// const myDog = {
+//   name: 'Coder',
+//   legs: 4,
+//   tails: 1,
+//   friends: ['freeCodeCamp Campers'],
+// };
+
+// // Only change code below this line
+// myDog.name = 'Happy Coder';
+// console.log(myDog.name);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Add New Properties to a JavaScript Object
+// const myDog = {
+//   name: 'Happy Coder',
+//   legs: 4,
+//   tails: 1,
+//   friends: ['freeCodeCamp Campers'],
+// };
+
+// myDog.bark1 = 'woof';
+// myDog['bark2'] = 'woof-woof';
+// console.log(myDog, myDog.bark1, myDog['bark2']);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Delete Properties from a JavaScript Object
+// // Setup
+// const myDog = {
+//   name: 'Happy Coder',
+//   legs: 4,
+//   tails: 1,
+//   friends: ['freeCodeCamp Campers'],
+//   bark: 'woof',
+// };
+
+// // Only change code below this line
+// delete myDog.tails;
+// console.log(myDog);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
