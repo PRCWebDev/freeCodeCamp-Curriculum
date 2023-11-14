6b5e5dd1df6bbd9ddf162f3c6359f839d8f01575
PRCWebDev <webdevprc@gmail.com>
2023-11-14 11:20:39 +0200

    completed more challenges from the Basic JavaScript Course

--- JavaScript Algorithms and Data Structures Certification/Basic JavaScript/script.js
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/script.js
@@ -1105,54 +1105,262 @@ function trueOrFalse(wasThatTrue) {
 
 ////////////////////////////////////////
 //  ? Using Objects for Lookups
+// // Setup
+// function phoneticLookup(val) {
+//   let result = '';
+
+//   // Only change code below this line
+//   // switch (val) {
+//   //   case 'alpha':
+//   //     result = 'Adams';
+//   //     break;
+//   //   case 'bravo':
+//   //     result = 'Boston';
+//   //     break;
+//   //   case 'charlie':
+//   //     result = 'Chicago';
+//   //     break;
+//   //   case 'delta':
+//   //     result = 'Denver';
+//   //     break;
+//   //   case 'echo':
+//   //     result = 'Easy';
+//   //     break;
+//   //   case 'foxtrot':
+//   //     result = 'Frank';
+//   // }
+
+//   const lookup = {
+//     alpha: 'Adams',
+//     bravo: 'Boston',
+//     charlie: 'Chicago',
+//     delta: 'Denver',
+//     echo: 'Easy',
+//     foxtrot: 'Frank',
+//   };
+
+//   result = lookup[val];
+//   // Only change code above this line
+//   return result;
+// }
+
+// phoneticLookup('charlie');
+// console.log(phoneticLookup('charlie'));
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Testing Objects for Properties
+// function checkObj(obj, checkProp) {
+//   // Only change code below this line
+//   if (obj.hasOwnProperty(checkProp)) return obj[checkProp];
+//   return 'Not Found';
+//   // Only change code above this line
+// }
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Manipulating Complex Objects
+// const myMusic = [
+//   {
+//     artist: 'Billy Joel',
+//     title: 'Piano Man',
+//     release_year: 1973,
+//     formats: ['CD', '8T', 'LP'],
+//     gold: true,
+//   },
+// ];
+
+// myMusic.push({
+//   artist: 'Basd',
+//   title: 'Pasd',
+//   release_year: 1983,
+//   formats: ['CD1', '8T1', 'LP1'],
+//   gold: false,
+// });
+
+// console.log(myMusic);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Accessing Nested Objects
+// const myStorage = {
+//   car: {
+//     inside: {
+//       'glove box': 'maps',
+//       'passenger seat': 'crumbs',
+//     },
+//     outside: {
+//       trunk: 'jack',
+//     },
+//   },
+// };
+
+// const gloveBoxContents = myStorage.car.inside['glove box'];
+// console.log(gloveBoxContents);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Accessing Nested Arrays
+// const myPlants = [
+//   {
+//     type: 'flowers',
+//     list: ['rose', 'tulip', 'dandelion'],
+//   },
+//   {
+//     type: 'trees',
+//     list: ['fir', 'pine', 'birch'],
+//   },
+// ];
+
+// const secondTree = myPlants[1].list[1];
+// console.log(secondTree);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Record Collection
+// // Setup
+// const recordCollection = {
+//   2548: {
+//     albumTitle: 'Slippery When Wet',
+//     artist: 'Bon Jovi',
+//     tracks: ['Let It Rock', 'You Give Love a Bad Name'],
+//   },
+//   2468: {
+//     albumTitle: '1999',
+//     artist: 'Prince',
+//     tracks: ['1999', 'Little Red Corvette'],
+//   },
+//   1245: {
+//     artist: 'Robert Palmer',
+//     tracks: [],
+//   },
+//   5439: {
+//     albumTitle: 'ABBA Gold',
+//   },
+// };
+
+// function updateRecords(records, id, prop, value) {
+//   if (!value) {
+//     delete records[id][prop];
+//   } else if (prop != 'tracks') {
+//     records[id][prop] = value;
+//   } else {
+//     if (!records[id].hasOwnProperty('tracks')) {
+//       records[id].tracks = [];
+//     }
+//     records[id].tracks.push(value);
+//   }
+
+//   console.log(records);
+//   return records;
+// }
+
+// updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+// updateRecords(recordCollection, 5439, 'tracks', 'Take a Chance on Me');
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Iterate with JavaScript While Loops
+// // Setup
+// const myArray = [];
+
+// // Only change code below this line
+// let i = 5;
+// while (i >= 0) {
+//   myArray.push(i);
+//   i--;
+// }
+// console.log(myArray);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Iterate with JavaScript For Loops
+// // Setup
+// const myArray = [];
+
+// // Only change code below this line
+// for (let i = 1; i <= 5; i++) {
+//   myArray.push(i);
+// }
+// console.log(myArray);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Iterate Odd Numbers With a For Loop
+// // Setup
+// const myArray = [];
+
+// // Only change code below this line
+// for (let i = 1; i < 10; i++) {
+//   if (i % 2 !== 0) {
+//     myArray.push(i);
+//   }
+// }
+// console.log(myArray);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Count Backwards With a For Loop
+// // Setup
+// const myArray = [];
+
+// // Only change code below this line
+// for (let i = 9; i > 0; i--) {
+//   if (i % 2 !== 0) {
+//     myArray.push(i);
+//   }
+// }
+// console.log(myArray);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Iterate Through an Array with a For Loop
+// // Setup
+// const myArr = [2, 3, 4, 5, 6];
+
+// // Only change code below this line
+// let total = 0;
+// for (let i = 0; i < myArr.length; i++) {
+//   total += myArr[i];
+// }
+// console.log(total);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Nesting For Loops
+// function multiplyAll(arr) {
+//   let product = 1;
+//   // Only change code below this line
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//       product *= arr[i][j];
+//     }
+//   }
+//   console.log(product);
+//   // Only change code above this line
+//   return product;
+// }
+
+// multiplyAll([
+//   [1, 2],
+//   [3, 4],
+//   [5, 6, 7],
+// ]);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Iterate with JavaScript Do...While Loops
+// // Setup
+// const myArray = [];
+// let i = 10;
+
+// // Only change code below this line
+// do {
+//   myArray.push(i);
+//   i++;
+// } while (i < 5);
+
+// console.log(myArray);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
