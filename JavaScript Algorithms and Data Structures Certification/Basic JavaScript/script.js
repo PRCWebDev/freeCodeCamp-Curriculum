7642bb23d6c6b013d0383e2d8152ec16bc918c68
PRCWebDev <webdevprc@gmail.com>
2023-11-14 15:09:55 +0200

    completed all the challenges from the Basic JavaScript Course

--- JavaScript Algorithms and Data Structures Certification/Basic JavaScript/script.js
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/script.js
@@ -1365,44 +1365,170 @@ function trueOrFalse(wasThatTrue) {
 
 ////////////////////////////////////////
 //  ? Replace Loops using Recursion
+// function sum(arr, n) {
+//   // Only change code below this line
+//   if (n <= 0) {
+//     return 0;
+//   } else {
+//     return sum(arr, n - 1) + arr[n - 1];
+//   }
+//   // Only change code above this line
+// }
+
+// console.log(sum([1], 0));
+// console.log(sum([2, 3, 4], 1));
+// console.log(sum([2, 3, 4, 5], 3));
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Profile Lookup
+// // Setup
+// const contacts = [
+//   {
+//     firstName: 'Akira',
+//     lastName: 'Laine',
+//     number: '0543236543',
+//     likes: ['Pizza', 'Coding', 'Brownie Points'],
+//   },
+//   {
+//     firstName: 'Harry',
+//     lastName: 'Potter',
+//     number: '0994372684',
+//     likes: ['Hogwarts', 'Magic', 'Hagrid'],
+//   },
+//   {
+//     firstName: 'Sherlock',
+//     lastName: 'Holmes',
+//     number: '0487345643',
+//     likes: ['Intriguing Cases', 'Violin'],
+//   },
+//   {
+//     firstName: 'Kristian',
+//     lastName: 'Vos',
+//     number: 'unknown',
+//     likes: ['JavaScript', 'Gaming', 'Foxes'],
+//   },
+// ];
+
+// function lookUpProfile(name, prop) {
+//   // Only change code below this line
+//   for (let i = 0; i < contacts.length; i++) {
+//     if (contacts[i].firstName === name) {
+//       if (contacts[i].hasOwnProperty(prop)) {
+//         return contacts[i][prop];
+//       }
+//       return 'No such property';
+//     }
+//   }
+//   return 'No such contact';
+//   // Only change code above this line
+// }
+
+// console.log(lookUpProfile('Akira', 'likes'));
+// console.log(lookUpProfile('Bob', 'number'));
+// console.log(lookUpProfile('Akira', 'address'));
+// console.log(lookUpProfile('Kristian', 'lastName'));
+// console.log(lookUpProfile('Sherlock', 'likes'));
+// console.log(lookUpProfile('Harry', 'likes'));
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Generate Random Fractions with JavaScript
+// function randomFraction() {
+//   // Only change code below this line
+
+//   return Math.random();
+
+//   // Only change code above this line
+// }
+// console.log(randomFraction());
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Generate Random Whole Numbers with JavaScript
+// function randomWholeNum() {
+//   return Math.floor(Math.random() * 10);
+// }
+
+// console.log(randomWholeNum());
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Generate Random Whole Numbers within a Range
+// function randomRange(myMin, myMax) {
+//   return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+// }
+
+// console.log(randomRange(1, 10));
+// console.log(randomRange(1, 2));
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Use the parseInt Function
+// function convertToInteger(str) {
+//   return parseInt(str);
+// }
+
+// console.log(convertToInteger('0056'));
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Use the parseInt Function with a Radix
+// function convertToInteger(str) {
+//   return parseInt(str, 2);
+// }
+
+// console.log(convertToInteger('10011'));
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Use the Conditional (Ternary) Operator
+// function checkEqual(a, b) {
+//   return a === b ? 'Equal' : 'Not Equal';
+// }
+
+// checkEqual(1, 2);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Use Multiple Conditional (Ternary) Operators
+// function checkSign(num) {
+//   return num > 0 ? 'positive' : num !== 0 ? 'negative' : 'zero';
+// }
+
+// checkSign(10);0
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Use Recursion to Create a Countdown
+// Only change code below this line
+// function countdown(n) {
+//   if (n < 1) {
+//     return [];
+//   } else {
+//     const countArray = countdown(n - 1);
+//     // console.log(n);
+//     countArray.unshift(n);
+//     return countArray;
+//   }
+// }
+// console.log(countdown(5));
+
+// Only change code above this line
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Use Recursion to Create a Range of Numbers
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum < startNum) {
+    return [];
+  } else {
+    const countArray = rangeOfNumbers(startNum, endNum - 1);
+    // console.log(startNum, endNum);
+    countArray.push(endNum);
+    return countArray;
+  }
+}
+
+console.log(rangeOfNumbers(1, 5));
 ////////////////////////////////////////
