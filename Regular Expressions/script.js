593e142dc7e9570a6b9484f8bc3f2b46f77bc0e7
PRCWebDev <webdevprc@gmail.com>
2023-11-22 10:59:20 +0200

    completed some of the challenges

--- Regular Expressions/script.js
+++ Regular Expressions/script.js
@@ -20,62 +20,129 @@ In this course, you'll learn how to use special characters, capture groups, posi
 
 ////////////////////////////////////////
 //  ? Using the Test Method
+// let myString = 'Hello, World!';
+// let myRegex = /Hello/;
+// let result = myRegex.test(myString); // Change this line
 
+// console.log(result);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Match Literal Strings
+// let waldoIsHiding = 'Somewhere Waldo is hiding in this text.';
+// let waldoRegex = /Waldo/; // Change this line
+// let result = waldoRegex.test(waldoIsHiding);
 
+// console.log(result);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Match a Literal String with Different Possibilities
+// let petString = 'James has a pet cat.';
+// let petRegex = /dog|cat|bird|fish/; // Change this line
+// let result = petRegex.test(petString);
 
+// console.log(result);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Ignore Case While Matching
+// let myString = 'freeCodeCamp';
+// let fccRegex = /FrEeCoDeCamp/i; // Change this line
+// let result = fccRegex.test(myString);
 
+// console.log(result);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Extract Matches
+// let extractStr = "Extract the word 'coding' from this string.";
+// let codingRegex = /coding/; // Change this line
+// let result = extractStr.match(codingRegex); // Change this line
 
+// console.log(result);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Find More Than the First Match
+// let testStr = 'Repeat, Repeat, Repeat';
+// let ourRegex = /Repeat/;
+// console.log(testStr.match(ourRegex));
 
+// let repeatRegex = /Repeat/g;
+// testStr.match(repeatRegex);
+// console.log(testStr.match(repeatRegex));
+
+// let twinkleStar = 'Twinkle, twinkle, little star';
+// !!! You can have multiple flags on your regex like /search/gi
+// let starRegex = /Twinkle/gi; // Change this line !!!
+// let result = twinkleStar.match(starRegex); // Change this line
+// console.log(result);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Match Anything with Wildcard Period
+// let exampleStr = "Let's have fun with regular expressions!";
+// let unRegex = /un./; // Change this line
+// let result = unRegex.test(exampleStr);
 
+// console.log(result);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Match Single Character with Multiple Possibilities
+// let bigStr = 'big';
+// let bagStr = 'bag';
+// let bugStr = 'bug';
+// let bogStr = 'bog';
+// let bgRegex = /b[aiu]g/;
+// console.log(bigStr.match(bgRegex));
+// console.log(bagStr.match(bgRegex));
+// console.log(bugStr.match(bgRegex));
+// console.log(bogStr.match(bgRegex));
 
+// let quoteSample =
+//   'Beware of bugs in the above code; I have only proved it correct, not tried it.';
+// let vowelRegex = /[aeiou]/gi; // Change this line !!! Note: Be sure to match both upper- and lowercase vowels. (aka (/i))
+// let result = quoteSample.match(vowelRegex); // Change this line
+
+// console.log(result);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Match Letters of the Alphabet
+// let quoteSample = 'The quick brown fox jumps over the lazy dog.';
+// let alphabetRegex = /[a-z]/gi; // Change this line !!! Note: Be sure to match both upper- and lowercase. (aka (/i))
+// let result = quoteSample.match(alphabetRegex); // Change this line
 
+// console.log(result);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Match Numbers and Letters of the Alphabet
+// let quoteSample = 'Blueberry 3.141592653s are delicious.';
+// let myRegex = /[h-s2-6]/gi; // Change this line
+// let result = quoteSample.match(myRegex); // Change this line
 
+// console.log(result);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Match Single Characters Not Specified
+// let quoteSample = '3 blind mice.';
+// let myRegex = /[^0-9aeiou]/gi; // Change this line
+// let result = quoteSample.match(myRegex); // Change this line
 
+// console.log(result);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
 //  ? Match Characters that Occur One or More Times
+let difficultSpelling = 'Mississippi';
+let myRegex = /s+/g; // Change this line
+let result = difficultSpelling.match(myRegex);
 
+console.log(result);
 ////////////////////////////////////////
 
 ////////////////////////////////////////
